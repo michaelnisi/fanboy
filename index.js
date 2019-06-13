18d37d0102089521ad288296a8c477050844cd52
Michael Nisi <michael.nisi@gmail.com>
2019-06-13 10:19:01 +0200

    Move lookup

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const util = require('util')
 const { FanboyTransform, defaults, mkpath, guid } = require('./lib/stream')
 const { Search } = require('./lib/search')
 const { SearchTerms } = require('./lib/suggest')
+const { Lookup } = require('./lib/lookup')
 const { createResultsParser } = require('./lib/http')
 
 const {
   createDatabase,
   close,
-  resultForID,
   isStale,
   keyStream,
   termOp,
@@ -62,42 +62,6 @@ if (TEST) {
   Fanboy.prototype.close = function (cb) {
     close(this.db, cb)
   }
-}
-
-// Lookup item in store
-function Lookup (db, opts) {
-  if (!(this instanceof Lookup)) return new Lookup(db, opts)
-  FanboyTransform.call(this, db, opts)
-  this.path = '/lookup'
-}
-util.inherits(Lookup, FanboyTransform)
-
-// - chunk iTunes ID (e.g. '537879700')
-Lookup.prototype._transform = function (chunk, enc, cb) {
-  if (this.db.isClosed()) {
-    return cb(new Error('fanboy: database closed'))
-  }
-
-  const db = this.db
-  const guid = this.decode(chunk)
-
-  debug('looking up: %s', guid)
-
-  if (!parseInt(guid, 10)) {
-    this.emit('error', new Error('fanboy: guid ' + guid + ' is not a number'))
-    return cb()
-  }
-
-  resultForID(db, guid, (er, value) => {
-    if (er) {
-      if (er.notFound) {
-        return this._request(guid, cb)
-      }
-    } else if (value !== undefined) {
-      this.use(value)
-    }
-    cb(er)
-  })
 }
 
 if (TEST) {
