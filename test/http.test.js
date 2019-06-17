52bc4eadc8e3f52014c033ccdc266fcb2a7069c1
Michael Nisi <michael.nisi@gmail.com>
2019-06-17 11:32:58 +0200

    Add some notes more tests

--- test/http.test.js
+++ test/http.test.js
@@ -4,13 +4,21 @@ const common = require('./lib/common')
 const http = require('http')
 const test = require('tap').test
 const { URL } = require('url')
-const { createPath } = require('../lib/v2/http')
+const { createPath, request } = require('../lib/v2/http')
 
 test('creating iTunes API path', t => {
   t.is(createPath('/search', 'apple'), '/search?media=all&country=us&term=apple')
   t.is(createPath('/lookup', '123'), '/lookup?id=123')
   t.throws(() => { createPath('/hello', 'dog') })
   t.end()
+})
+
+test('aborted', t => {
+  const req = request('/search', 'abortion', (er, res) => {
+    t.is(er.message, 'fanboy: request aborted')
+    t.end()
+  })
+  req.abort()
 })
 
 function codesBetween (smaller, larger) {
--- test/search.test.js
+++ test/search.test.js
@@ -126,16 +126,16 @@ test('unexpected HTTP status code', t => {
 test('failed request', t => {
   const cache = common.freshCache()
 
-  const { hostname, port } = env
+  const { hostname, port } = env()
 
-  env.hostname = 'xxx'
-  env.port = 12345
+  process.env.FANBOY_HOSTNAME = 'xxx'
+  process.env.FANBOY_PORT = 12345
 
   cache.ssearch('hopeless', (er, results) => {
     t.is(er.message, 'fanboy: getaddrinfo ENOTFOUND xxx xxx:12345')
 
-    env.hostname = hostname
-    env.port = port
+    process.env.FANBOY_HOSTNAME = hostname
+    process.env.FANBOY_PORT = port
 
     common.teardown(cache, () => {
       t.pass('should teardown')
@@ -156,6 +156,27 @@ test('parse error', t => {
 
   cache.ssearch('dog', (er, results) => {
     t.is(er.message, 'Invalid JSON (Unexpected "h" at position 0 in state STOP)')
+    t.ok(scope.isDone())
+    common.teardown(cache, () => {
+      t.pass('should teardown')
+      t.end()
+    })
+  })
+})
+
+test('socket timeout', t => {
+  const scope = nock('http://itunes.apple.com')
+
+  scope.get('/search?media=podcast&country=us&term=' + 'dog')
+    .socketDelay(5000)
+    .reply(200, (uri, body) => {
+      return 'hello, here dog'
+    })
+
+  const cache = common.freshCache()
+
+  cache.ssearch('dog', (er, results) => {
+    t.is(er.message, 'fanboy: socket hang up')
     t.ok(scope.isDone())
     common.teardown(cache, () => {
       t.pass('should teardown')
